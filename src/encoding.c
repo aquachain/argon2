P-H-C/phc-winner-argon2

commit 6887dae4889b36df87e50623dc8035f9fcfc0663

decoding and argon2_ codes unifiedd
Fixed a bug that confuses the decoding error codes and argon2 error
codes. Now decoding and encoding produce argon2 error codes

src/encoding.c
@@ -250,7 +250,7 @@ int decode_string(argon2_context *ctx, const char *str, argon2_type type) {
     do {                                                                       \
         size_t cc_len = strlen(prefix);                                        \
         if (strncmp(str, prefix, cc_len) != 0) {                               \
-            return 0;                                                          \
+            return ARGON2_DECODING_FAIL;                                                          \
         }                                                                      \
         str += cc_len;                                                         \
     } while ((void)0, 0)
@@ -272,6 +272,7 @@ int decode_string(argon2_context *ctx, const char *str, argon2_type type) {
         str = decode_decimal(str, &dec_x);                                     \
         if (str == NULL) {                                                     \
             return 0;                                                          \
+            return ARGON2_DECODING_FAIL;                                                          \
         }                                                                      \
         (x) = dec_x;                                                           \
     } while ((void)0, 0)
@@ -281,7 +282,7 @@ int decode_string(argon2_context *ctx, const char *str, argon2_type type) {
         size_t bin_len = (max_len);                                            \
         str = from_base64(buf, &bin_len, str);                                 \
         if (str == NULL || bin_len > UINT32_MAX) {                             \
-            return 0;                                                          \
+            return ARGON2_DECODING_FAIL;                                                          \
         }                                                                      \
         (len) = (uint32_t)bin_len;                                             \
     } while ((void)0, 0)
@@ -311,20 +312,25 @@ int decode_string(argon2_context *ctx, const char *str, argon2_type type) {
 
     CC_opt(",data=", BIN(ctx->ad, maxadlen, ctx->adlen));
     if (*str == 0) {
-        return 1;
+        return ARGON2_OK;
     }
     CC("$");
     BIN(ctx->salt, maxsaltlen, ctx->saltlen);
     if (*str == 0) {
-        return 1;
+        return ARGON2_OK;
     }
     CC("$");
     BIN(ctx->out, maxoutlen, ctx->outlen);
-    if (validate_inputs(ctx) != ARGON2_OK) {
-        return 0;
+	int validation_result = validate_inputs(ctx);
+    if (validation_result != ARGON2_OK) {
+        return validation_result;
     }
-    return *str == 0;
-
+	if (*str == 0) {
+		return ARGON2_OK;
+	}
+	else {
+		return ARGON2_DECODING_FAIL;
+	}
 #undef CC
 #undef CC_opt
 #undef DECIMAL
@@ -337,7 +343,7 @@ int encode_string(char *dst, size_t dst_len, argon2_context *ctx,
     do {                                                                       \
         size_t pp_len = strlen(str);                                           \
         if (pp_len >= dst_len) {                                               \
-            return 0;                                                          \
+            return ARGON2_ENCODING_FAIL;                                                          \
         }                                                                      \
         memcpy(dst, str, pp_len + 1);                                          \
         dst += pp_len;                                                         \
@@ -355,7 +361,7 @@ int encode_string(char *dst, size_t dst_len, argon2_context *ctx,
     do {                                                                       \
         size_t sb_len = to_base64(dst, dst_len, buf, len);                     \
         if (sb_len == (size_t)-1) {                                            \
-            return 0;                                                          \
+            return ARGON2_ENCODING_FAIL;                                                          \
         }                                                                      \
         dst += sb_len;                                                         \
         dst_len -= sb_len;                                                     \
@@ -366,10 +372,10 @@ int encode_string(char *dst, size_t dst_len, argon2_context *ctx,
     else if (type == Argon2_d)
         SS("$argon2d$m=");
     else
-        return 0;
+        return ARGON2_ENCODING_FAIL;
 
     if (validate_inputs(ctx) != ARGON2_OK) {
-        return 0;
+        return validate_inputs(ctx);
     }
     SX(ctx->m_cost);
     SS(",t=");
@@ -383,17 +389,17 @@ int encode_string(char *dst, size_t dst_len, argon2_context *ctx,
     }
 
     if (ctx->saltlen == 0)
-        return 1;
+        return ARGON2_OK;
 
     SS("$");
     SB(ctx->salt, ctx->saltlen);
 
     if (ctx->outlen == 0)
-        return 1;
+        return ARGON2_OK;
 
     SS("$");
     SB(ctx->out, ctx->outlen);
-    return 1;
+    return ARGON2_OK;
 
 #undef SS
 #undef SX

src/argon2.c
@@ -227,7 +227,7 @@ int argon2_hash(const uint32_t t_cost, const uint32_t m_cost,
 
     /* if encoding requested, write it */
     if (encoded && encodedlen) {
-        if (!encode_string(encoded, encodedlen, &context, type)) {
+        if (encode_string(encoded, encodedlen, &context, type)!=ARGON2_OK) {
 			secure_wipe_memory(out, hashlen); /* wipe buffers if error */
 			secure_wipe_memory(encoded, encodedlen);
             free(out);
@@ -319,7 +319,7 @@ int argon2_verify(const char *encoded, const void *pwd, const size_t pwdlen,
         return ARGON2_MEMORY_ALLOCATION_ERROR;
     }
 	int decode_result = decode_string(&ctx, encoded, type);
-    if(decode_result != 1) {
+    if(decode_result != ARGON2_OK) {
         free(ctx.ad);
         free(ctx.salt);
         free(ctx.out);

src/encoding.h
@@ -11,13 +11,13 @@
 * encode an Argon2 hash string into the provided buffer. 'dst_len'
 * contains the size, in characters, of the 'dst' buffer; if 'dst_len'
 * is less than the number of required characters (including the
-* terminating 0), then this function returns 0.
+* terminating 0), then this function returns ARGON2_ENCODING_ERROR.
 *
 * if ctx->outlen is 0, then the hash string will be a salt string
 * (no output). if ctx->saltlen is also 0, then the string will be a
 * parameter-only string (no salt and no output).
 *
-* on success, 1 is returned.
+* on success, ARGON2_OK is returned.
 *
 * No other parameters are checked
 */
@@ -29,14 +29,7 @@ int encode_string(char *dst, size_t dst_len, argon2_context *ctx,
 * Decodes an Argon2 hash string into the provided structure 'ctx'.
 * The fields ctx.saltlen, ctx.adlen, ctx.outlen set the maximal salt, ad, out length values 
 * that are allowed; invalid input string causes an error
-* Returned value is 1 on success, 0 on error.
-*
-* WARNING: the procedure rejects some inputs that are valid outputs of encode_string(), see below
-* Allows m_cost and t_cost between 1 and 2^32-1 (hard-coded values), 
-* parallelism between 1 and ARGON2_MAX_DECODED_LANES.
-* Also rejects m_cost if smaller than  8 times parallelism.
-* Rejects salts shorter than ARGON2_MIN_DECODED_SALT_LEN bytes.
-* Rejects hashes shorter than ARGON2_MIN_DECODED_OUT_LEN bytes.
+* Returned value is ARGON2_OK on success, other ARGON2_ codes on error.
 */
 int decode_string(argon2_context *ctx, const char *str, argon2_type type);
 

src/run.c
@@ -126,7 +126,63 @@ static void run(uint8_t *out, char *pwd, uint8_t *salt, uint32_t t_cost,
     printf("Verification ok\n");
 }
 
-int main(int argc, char *argv[]) {
+#define HASHLEN 32
+#define SALTLEN 16
+#define PWD "password"
+
+int main(void)
+{
+	char hash_i[HASHLEN * 100];
+	char hash_d[HASHLEN * 100];
+
+	uint8_t salt[SALTLEN];
+	memset(salt, 0x00, SALTLEN);
+
+	uint8_t *pwd = (uint8_t *)strdup(PWD);
+	uint32_t pwdlen = strlen((char *)pwd);
+
+	uint32_t t_cost = 2;            // 1-pass computation
+	uint32_t m_cost = (1 << 16);      // 64 mebibytes memory usage
+	uint32_t parallelism = 1;       // number of threads and lanes
+
+	argon2i_hash_encoded(
+		t_cost, m_cost,
+		parallelism,
+		pwd, pwdlen,
+		salt, SALTLEN,
+		HASHLEN,
+		hash_i, HASHLEN * 100
+		);
+	argon2d_hash_encoded(
+		t_cost, m_cost,
+		parallelism,
+		pwd, pwdlen,
+		salt, SALTLEN,
+		HASHLEN,
+		hash_d, HASHLEN * 100
+		);
+
+	printf("hash_i: %s\n", hash_i);
+	printf("hash_d: %s\n\n", hash_d);
+
+	int rv;
+
+	rv = argon2i_verify(hash_i, "hello", 5);
+	printf("wrong pwd, correct type (both i): %d\n", rv);
+	rv = argon2d_verify(hash_d, "hello", 5);
+	printf("wrong pwd, correct type (both d): %d\n", rv);
+	rv = argon2i_verify(hash_d, "hello", 5);
+	printf("wrong pwd, wrong type (i/d): %d\n", rv);
+	rv = argon2d_verify(hash_i, "hello", 5);
+	printf("wrong pwd, wrong type (d/i): %d\n", rv);
+	if (rv == 0) {
+		return 1;
+	}
+
+	return 0;
+}
+
+int main2(int argc, char *argv[]) {
     unsigned char out[OUT_LEN];
     uint32_t m_cost = 1 << LOG_M_COST_DEF;
     uint32_t t_cost = T_COST_DEF;

aquachain/argon2

commit b09ffdd2db49312a6a105520f612831e59e1df67

Fix bug introduced in 6887dae4889b36df87e50623dc8035f9fcfc0663.

src/encoding.c
@@ -272,7 +272,6 @@ int decode_string(argon2_context *ctx, const char *str, argon2_type type) {
         unsigned long dec_x;                                                   \
         str = decode_decimal(str, &dec_x);                                     \
         if (str == NULL) {                                                     \
-            return 0;                                                          \
             return ARGON2_DECODING_FAIL;                                       \
         }                                                                      \
         (x) = dec_x;                                                           \