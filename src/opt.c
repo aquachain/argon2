a25423443e8b6626c738e5046a58fa81a34ecabd
cryptogone <cryptogone.dev@gmail.com>
2018-05-16 17:08:45 +0200

    put back argon2id code

--- src/opt.c
+++ src/opt.c
@@ -184,7 +184,7 @@ static void next_addresses(block *address_block, block *input_block) {
 }
 
 void fill_segment(const argon2_instance_t *instance,
-                  argon2_position_t position) {
+    argon2_position_t position) {
     block *ref_block = NULL, *curr_block = NULL;
     block address_block, input_block;
     uint64_t pseudo_rand, ref_index, ref_lane;
@@ -197,36 +197,48 @@ void fill_segment(const argon2_instance_t *instance,
 #else
     __m128i state[ARGON2_OWORDS_IN_BLOCK];
 #endif
+    int data_independent_addressing;
+
     if (instance == NULL) {
         return;
     }
 
-    init_block_value(&input_block, 0);
+    data_independent_addressing =
+        (instance->type == Argon2_i) ||
+        (instance->type == Argon2_id && (position.pass == 0) &&
+        (position.slice < ARGON2_SYNC_POINTS / 2));
 
-    input_block.v[0] = position.pass;
-    input_block.v[1] = position.lane;
-    input_block.v[2] = position.slice;
-    input_block.v[3] = instance->memory_blocks;
-    input_block.v[4] = instance->passes;
-    input_block.v[5] = instance->type;
+    if (data_independent_addressing) {
+        init_block_value(&input_block, 0);
+
+        input_block.v[0] = position.pass;
+        input_block.v[1] = position.lane;
+        input_block.v[2] = position.slice;
+        input_block.v[3] = instance->memory_blocks;
+        input_block.v[4] = instance->passes;
+        input_block.v[5] = instance->type;
+    }
 
     starting_index = 0;
 
     if ((0 == position.pass) && (0 == position.slice)) {
         starting_index = 2; /* we have already generated the first two blocks */
 
-        /* Don't forget to generate the first block of addresses: */
-        next_addresses(&address_block, &input_block);
+                            /* Don't forget to generate the first block of addresses: */
+        if (data_independent_addressing) {
+            next_addresses(&address_block, &input_block);
+        }
     }
 
     /* Offset of the current block */
     curr_offset = position.lane * instance->lane_length +
-                  position.slice * instance->segment_length + starting_index;
+        position.slice * instance->segment_length + starting_index;
 
     if (0 == curr_offset % instance->lane_length) {
         /* Last block in this lane */
         prev_offset = curr_offset + instance->lane_length - 1;
-    } else {
+    }
+    else {
         /* Previous block */
         prev_offset = curr_offset - 1;
     }
@@ -234,7 +246,7 @@ void fill_segment(const argon2_instance_t *instance,
     memcpy(state, ((instance->memory + prev_offset)->v), ARGON2_BLOCK_SIZE);
 
     for (i = starting_index; i < instance->segment_length;
-         ++i, ++curr_offset, ++prev_offset) {
+        ++i, ++curr_offset, ++prev_offset) {
         /*1.1 Rotating prev_offset if needed */
         if (curr_offset % instance->lane_length == 1) {
             prev_offset = curr_offset - 1;
@@ -242,10 +254,15 @@ void fill_segment(const argon2_instance_t *instance,
 
         /* 1.2 Computing the index of the reference block */
         /* 1.2.1 Taking pseudo-random value from the previous block */
-        if (i % ARGON2_ADDRESSES_IN_BLOCK == 0) {
-            next_addresses(&address_block, &input_block);
+        if (data_independent_addressing) {
+            if (i % ARGON2_ADDRESSES_IN_BLOCK == 0) {
+                next_addresses(&address_block, &input_block);
+            }
+            pseudo_rand = address_block.v[i % ARGON2_ADDRESSES_IN_BLOCK];
         }
-        pseudo_rand = address_block.v[i % ARGON2_ADDRESSES_IN_BLOCK];
+        else {
+            pseudo_rand = instance->memory[prev_offset].v[0];
+        }
 
         /* 1.2.2 Computing the lane of the reference block */
         ref_lane = ((pseudo_rand >> 32)) % instance->lanes;
@@ -256,16 +273,21 @@ void fill_segment(const argon2_instance_t *instance,
         }
 
         /* 1.2.3 Computing the number of possible reference block within the
-         * lane.
-         */
+        * lane.
+        */
         position.index = i;
         ref_index = index_alpha(instance, &position, pseudo_rand & 0xFFFFFFFF,
-                                ref_lane == position.lane);
+            ref_lane == position.lane);
 
         /* 2 Creating a new block */
         ref_block =
             instance->memory + instance->lane_length * ref_lane + ref_index;
         curr_block = instance->memory + curr_offset;
-		fill_block(state, ref_block, curr_block, 0);
+        if (0 == position.pass) {
+            fill_block(state, ref_block, curr_block, 0);
+        }
+        else {
+            fill_block(state, ref_block, curr_block, 1);
+       }
     }
 }
